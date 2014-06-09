406addb908a8c6e2024d301754abffe9568e0666
Latrisha <alexandrapyshko@gmail.com>
2014-06-10 00:41:59 +0300

    some field deleted

--- src/effectiveproductionsystem/EffectiveProductionSystem.java
+++ src/effectiveproductionsystem/EffectiveProductionSystem.java
@@ -1,22 +1,106 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package effectiveproductionsystem;
 
-/**
- *
- * @author alexandra
- */
-public class EffectiveProductionSystem {
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class RequestPart
+{
+    private Instrument instrument;
+    private int time;
+    private int startTime = 0;
+    
+    public RequestPart(Instrument inst, int time)
+    {
+        this.instrument = inst;
+        this.time = time;
+    }
+    
+    public Instrument getInstrument()
+    {
+        return this.instrument;
+    }
+    
+    public int getTime()
+    {
+        return this.time;
+    }
+    
+    public int getStartTime()
+    {
+        return this.startTime;
+    }
+    
+    public void setStartTime(int time)
+    {
+        this.startTime = time;
+    }
+}
+
+class Instrument
+{
+    private String name;
+    private int count;
+    
+    public Instrument(String name, int count)
+    {
+        this.name = name;
+        this.count = count;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public int getCount()
+    {
+        return this.count;
+    }
+            
+}
+
+
+class Request
+{
+    private List<RequestPart> parts = new ArrayList<RequestPart>();
+    
+    public void addPart(Instrument instrument, int time)
+    {
+        parts.add(new RequestPart(instrument, time));
+        
+    }
+    
+    public List getParts()
+    {
+        return parts;
+    }
+    
+    
+}
+
+public class EffectiveProductionSystem
+{
+    private List<Request> requests = new ArrayList<Request>();
+    private List<Instrument> instruments = new ArrayList<Instrument>();
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+    public void save()
+    {
+        
+    }
+    
+    public void load()
+    {
+        
+    }
+    
+    public void processRequests()
+    {
+        
     }
     
 }
